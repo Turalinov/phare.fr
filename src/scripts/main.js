0581527f2c3ce42d3589b12694757a3d859c0c9c
Turalinov <rusltur_24@mail.ru>
2021-10-28 20:43:48 +0600

    programming menu hamburger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,12 @@
+(function() {
+  console.log(1);
+  const menuToggle = document.querySelector('.menu__toggle');
+  const menu = document.querySelector('.menu');
+  const body = document.querySelector('body')
 
-
-
-
+  menuToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    menu.classList.toggle('menu--opened')
+    body.classList.toggle('body--locked')
+  })
+})()
