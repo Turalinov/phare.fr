5edb5940d4ff627c5816e49569366173841b482f
Turalinov <rusltur_24@mail.ru>
2021-11-01 21:51:35 +0600

    entreprises:possibilities block adaptive

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "SRC_PATH": "src",
   "DIST_PATH": "docs",
-  "STYLES_LIBS": ['node_modules/normalize.css/normalize.css'],
-  "JS_LIBS": [],
+  "STYLES_LIBS": ['node_modules/normalize.css/normalize.css','node_modules/swiper/swiper-bundle.css' ],
+  "JS_LIBS": ['node_modules/swiper/swiper-bundle.js'],
 }
