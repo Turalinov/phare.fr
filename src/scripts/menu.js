5ac2cca6da22444da3439c533fc8cf1a6f08f488
Turalinov <rusltur_24@mail.ru>
2021-11-09 00:19:04 +0600

    step1 adaptive block

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,19 @@
 (function() {
   console.log('menu');
-  const menuToggle = document.querySelector('.menu__toggle');
-  const menu = document.querySelector('.menu');
-  const body = document.querySelector('body')
 
-  menuToggle.addEventListener('click', (e) => {
-    e.preventDefault();
-    menu.classList.toggle('menu--opened')
-    body.classList.toggle('body--locked')
-  })
+  const menuToggle = document.querySelector('.menu__toggle');
+
+  if (menuToggle) {
+    const menu = document.querySelector('.menu');
+    const body = document.querySelector('body')
+
+    menuToggle.addEventListener('click', (e) => {
+      e.preventDefault();
+      menu.classList.toggle('menu--opened')
+      body.classList.toggle('body--locked')
+    })
+
+  }
 })();
 
 (function() {
